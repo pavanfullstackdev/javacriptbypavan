bc3300bccaef3a3d3642babc378d3414f12ac4ad
Pavan Chandrakant Birari <pavan.fullstack.dev.tech@gmail.com>
2024-07-15 17:28:19 +0000

    Variables in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+// we can declare the variables using var,let,const
+
+const  petName = 'orangecat';
+let petId = '23';
+var petColor = 'black'
+eyeColor ='red';
+
+console.table([petName,petId,petColor,eyeColor])
+
+// We never use var to assign a variable to avoide block & functional scope errors
+
+//we can reassign the variable with var key
+
+//we can assign any variable without keyword but its bad practice
