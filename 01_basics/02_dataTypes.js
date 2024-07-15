4d7a82cf9eaffd6498961a14e52f9cd9fb0fd0de
Pavan Chandrakant Birari <pavan.fullstack.dev.tech@gmail.com>
2024-07-15 17:56:07 +0000

    Data types in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+// Premitive data types
+
+// Number   
+// BigInt
+// String
+// Boolean
+// undefined
+// null
+// Symbol
+
+//object
+
+
+// type of undefined is undefine
+// type of Null is object
+
+console.log(typeof (undefined))
+console.log(typeof (null))
