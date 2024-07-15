699bc7c03e5b4199dffabee2737eb127c782e75a
Pavan Chandrakant Birari <pavan.fullstack.dev.tech@gmail.com>
2024-07-15 18:12:46 +0000

    conversionOperatons

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = 33
+
+console.log(typeof (score));
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(valueInNumber)
+
+
+//"33" =>33
+//"33abc"=>Nan
+//true => 1;
+//false=>0
